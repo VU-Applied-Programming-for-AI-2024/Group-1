9cc1a738d38dc56dc3fb788919ee043ac2cf7ccb
y19tyagi <y.tyagi@student.vu.nl>
2024-06-13 12:39:39 +0200

    I made some changes

--- backend/backend.py
+++ backend/backend.py
@@ -7,13 +7,20 @@ def movies_search(movie_name:str):
     results = my_movie.search(movie_name)
     sys.stdout.reconfigure(encoding='utf-8')
     for i in results:
-    #     results_dict = results.__dict__
-    #     for key, value in results_dict.items():
-    #         if key == "_json":
-    #             for akey, avalue in value.items():
-    #                 if type(avalue) == list:
-    #                     for j in avalue:
-    #                         for key1, value1 in j.items():
-    #                             return ''
-        print(i.title, i.realsea)
-movies_search('Godzilla')
+        results_dict = results.__dict__
+        for key, value in results_dict.items():
+            if key == "_json":
+                for akey, avalue in value.items():
+                    if type(avalue) == list:
+                        for j in avalue:
+                            for key1, value1 in j.items():
+                                print(key1,value1)
+
+mdb = TMDb()
+mdb.api_key = '46cbbac59c440a0b0490ad2adad2b849'
+my_movie = Movie()
+search = my_movie.search("The Possession Of Mrs. Hyde")
+
+for res in search:
+    print(res.title)
+    
---
+++ frontend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/genre")
+def genre():
+    return render_template("genre.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
