5dae02b75253ea8179d16cbc45576f5cb55aaca9
ARcKP98 <pkartik4386@outlook.com>
2024-06-17 20:58:38 +0200

    Update review_page to show movie info.

--- frontend/app.py
+++ frontend/app.py
@@ -31,9 +31,25 @@ def login():
 def signup():
     return render_template("signup.html")
 
-@app.route("/info")
-def info():
-    return render_template("review_page.html")
+@app.route("/info/<movie_id>")
+def info(movie_id):
+    response = f'{base_url}/movie/{movie_id}?api_key={api_key}'
+    results = requests.get(response).json()
+    reponse2 = f'{base_url}/movie/{movie_id}/credits?api_key={api_key}'
+    results2 = requests.get(reponse2).json()
+    movie_info = {
+        'movie_title' : results.get('original_title'),
+        'plot' : results.get('overview'),
+        'poster_path' : f'https://image.tmdb.org/t/p/w500/{results.get("poster_path")}',
+        'release_date' : results.get('release_date'),
+        'score' : results.get('vote_average')
+    }
+    director_name = 'Not Available'
+    for person in results2.get('crew'):
+        if person['job'] == 'Director':
+            director_name = person['name']
+    movie_info['director'] = director_name
+    return render_template("review_page.html", data=movie_info)
 
 
 if __name__ == "__main__":
