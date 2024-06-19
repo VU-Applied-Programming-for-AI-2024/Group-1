95602bd08f5509e7b513075bdf5d675d0d0716ef
y19tyagi <y.tyagi@student.vu.nl>
2024-06-19 14:23:24 +0200

    made the search bar better

--- backend/search.py
+++ backend/search.py
@@ -1,34 +1,13 @@
 import sys
-from tmdbv3api import TMDb, Movie, TV 
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from tmdbv3api import TMDb, Movie, TV, Genre
+from flask import Flask
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy()
-# db.init_app(app)
+
 
 tmdb = TMDb()
 tmdb.api_key = '46cbbac59c440a0b0490ad2adad2b849'
 my_movie = Movie()
 my_tv = TV()
-
-# class SearchLog(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     search_query = db.Column(db.String(255), nullable=False)
-#     search_type = db.Column(db.String(50), nullable=False)
-
-# class SearchHistory(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     query = db.Column(db.String(200), nullable=False)
-#     results = db.Column(db.JSON, nullable=False)
-
-#     def __repr__(self) -> str:
-#         return  f"<SearchHistory(query='{self.query}', results='{self.results}')>"
-
-# with app.app_context():
-#     db.create_all()
-
 def search(query, filter_typ, genre_id, sort_opt):
     sys.stdout.reconfigure(encoding='utf-8')
     results = []
@@ -43,9 +22,6 @@ def search(query, filter_typ, genre_id, sort_opt):
     if sort_opt:
         results = sorting_it(results, sort_opt)  
 
-    # search_entry = SearchHistory(query=query, results=results)
-    # db.session.add(search_entry)
-    # db.session.commit()
 
     return results
 
@@ -63,25 +39,40 @@ def search_results(results, typ):
                     results_dct[l] = result.title
                 elif hasattr(result,'name'):
                     results_dct[l] = result.name    
+            
             if l == "overview":
-                results_dct[l] = result.overview
+                if hasattr(result,'overview'):
+                    results_dct[l] = result.overview
+                else:
+                    results_dct[l] = 'Overview not available'
+            
             if l == "rating":
-                results_dct[l] = result.vote_average
+                if hasattr(result,'vote_average'):
+                    results_dct[l] = result.vote_average
+                else:
+                    results_dct[l] = "Rating not available"
+
             if l == "poster_path":
-                results_dct[l] = result.poster_path
+                if hasattr(result, 'poster_path'):
+                    results_dct[l] = result.poster_path
+                else:
+                    results_dct[l] = 'Poster not available'
         results_dct["type"] = typ
         results_lst.append(results_dct)
     return results_lst
 def filter_genre(results, genre_id):
     filtered_lst = []
     for result in results:
-        if genre_id == result.genre_ids:
-            filtered_lst.append(result)
+        if hasattr(result,'genre_ids'):
+            if genre_id == result.genre_ids:
+                filtered_lst.append(result)
     return filtered_lst
 def sorting_it(results, sort_by):
-    if sort_by == 'date':
-        return sorted(results, key=lambda x: x.get('release_date', ''), reverse=True)
-    elif sort_by == 'rating':
-        return sorted(results, key=lambda x: x.get('rating', 0), reverse=True)
+    if sort_by == 'popularity':
+        results.sort(key=lambda x: x['popularity'], reverse=True)
+    elif sort_by == 'vote_average':
+        results.sort(key=lambda x: x['vote_average'], reverse=True)
+    elif sort_by == 'release_date':
+        results.sort(key=lambda x: x['release_date'])
     return results
 
